d81b1204b8903718479ecf6228b95b9e96ef77d9
AARTHI GD <sec24am014@sairamtap.edu.in>
2026-08-08 13:35:52 +0530

    Time: 2 ms (99.32%) | Memory: 44.4 MB (55.19%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        char[] chars = s.toCharArray();
+        int i=0; 
+        int j = chars.length-1;
+        while(i<j){
+            if(!Character.isLetterOrDigit(chars[i])){
+                i++;
+            }
+            else if(!Character.isLetterOrDigit(chars[j])){
+                j--;
+            }
+            else{
+                if(Character.toLowerCase(chars[i]) != Character.toLowerCase(chars[j])){
+                    return false;
+                }
+            
+                i++;
+                j--;
+            }
+        }
+        return true;
+        
+    }
+}
